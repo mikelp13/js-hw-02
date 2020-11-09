cebbd271275d45839a8f72997d13e40c2ac756d5
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-09 23:27:05 +0200

    clear test file

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
 
-debugger;
-
-let logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
-let login =  'Ajax';
-
-isLoginUnique (logins, login); 
-
-function isLoginUnique  (allLogins, login) {
-  'use strict';
-  // Write code under this line
-  let result = allLogins.includes(login);
-  return result;
-}
